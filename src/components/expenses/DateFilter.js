d545f5c6ac43fcad2bf5de6459f6d0f9d67d71d8
DESKTOP-BCRBV2A\marym <mehrikodes@gmail.com>
2022-12-02 17:48:09 +0900

    testFromWork - editing 'DateFilter'

--- src/components/expenses/DateFilter.js
+++ src/components/expenses/DateFilter.js
@@ -6,10 +6,10 @@ const DateFilter = () => {
     <div className='expenses-filter'>
 
         <select>
-            <option value="2019">2019</option>
             <option value="2020">2020</option>
             <option value="2021">2021</option>
             <option value="2022">2022</option>
+            <option value="2022">2023</option>
         </select>
     
     </div>
