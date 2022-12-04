8dd055a50873b7e0b0c4626d277cfb1822a1d03f
MAREEKO\marym <mehrikodes@gmail.com>
2022-12-04 19:36:36 +0900

    testFromHome -  editing 'DateFilter'

--- src/components/expenses/DateFilter.js
+++ src/components/expenses/DateFilter.js
@@ -1,17 +1,24 @@
  import "./DateFilter.css";
 
-const DateFilter = () => {
+const DateFilter = (props) => {
+
+    const onSubmitDateFilterHandler = (event) =>{
+        props.onSelectDateFilter(event.target.value);
+    }
 
     return(
+   
     <div className='expenses-filter'>
-
-        <select>
-            <option value="2020">2020</option>
+      <div className='expenses-filter__control'>
+        <label>Filter by year</label>
+        <select value={props.filterYear} onChange={onSubmitDateFilterHandler} >
+            <option value="2020" >2020</option>
             <option value="2021">2021</option>
             <option value="2022">2022</option>
             <option value="2022">2023</option>
         </select>
     
+       </div>
     </div>
     );
 }
--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,16 +1,30 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import DateFilter from "./DateFilter";
+import {useState} from 'react';
 
 function Expenses(props){
 
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const dateFilterHandler =(selectedDateFilter)=>{
+    const dateFilter= selectedDateFilter;
+    console.log("date filter date", dateFilter);
+    setFilteredYear(dateFilter);
+  }
+
     return(
+    <div>
+      
     <Card className="expenses">
-        <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date} />
+    <DateFilter filterYear={filteredYear} onSelectDateFilter={dateFilterHandler} />
+        <ExpenseItem  title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date} />
         <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date} />
         <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date} />
         <ExpenseItem title={props.expenses[3].title} amount={props.expenses[3].amount} date={props.expenses[3].date} />
       </Card>
+    </div>
 
     );
 }
