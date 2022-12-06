ce0da755267fd7a49d38fef10ddf71eee543fb69
DESKTOP-BCRBV2A\marym <mehrikodes@gmail.com>
2022-12-06 11:14:45 +0900

    Revert "testFromWork - improving 'Expenses'"
    This reverts commit 1fed7a0d3f03b9b75539779a2d2f5ee1fcfc1942.

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -7,19 +7,21 @@ import {useState} from 'react';
 function Expenses(props){
 
   const [filteredYear, setFilteredYear] = useState('2020');
-  
 
   const dateFilterHandler =(selectedDateFilter)=>{
+    const dateFilter= selectedDateFilter;
+    console.log("date filter date", dateFilter);
+    setFilteredYear(dateFilter);
+    const filteredArr= props.expenses.filter(ele => ele.date.getFullYear().toString() === dateFilter);
+    console.log("fitleredArray", filteredArr);
     
-    setFilteredYear(selectedDateFilter);
-  
   }
 
     return(
     <div>   
       <Card className="expenses">
         <DateFilter filterYear={filteredYear} onSelectDateFilter={dateFilterHandler} />
-          {props.expenses.filter(ele => ele.date.getFullYear().toString() === filteredYear).map(expense => 
+          {props.expenses.map(expense => 
           <ExpenseItem 
             title={expense.title} 
             amount={expense.amount}  
